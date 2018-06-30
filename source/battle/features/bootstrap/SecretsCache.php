bfe9ae7087fddc5d294cdc0ab43056dd7133574a
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-06-30 16:04:28 +0700

    add behat

---
+++ source/battle/features/bootstrap/SecretsCache.php
@@ -0,0 +1,15 @@
+<?php
+final class SecretsCache
+{
+    private $secretsMap = [];
+
+    public function setSecret($secret)
+    {
+        $this->secretsMap[$secret] = $secret;
+    }
+
+    public function getSecret($secret)
+    {
+        return $this->secretsMap[$secret];
+    }
+}
